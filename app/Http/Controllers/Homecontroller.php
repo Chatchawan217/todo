da83a4241ea562617540aa60f1d8eb6baaeee61a
Chatchawan <60143217@g.cmru.ac.th>
2020-07-18 16:09:32 +0700

    Todo

---
+++ app/Http/Controllers/Homecontroller.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Homecontroller extends Controller
+{
+    public function index(){
+        return view('home');
+    }
+
+    public function show($id){
+
+    }
+
+    public function create(){
+
+    }
+
+    public function store(){
+
+    }
+
+    public function update($id){
+
+    }
+
+    public function delete($id){
+
+    }
+}
